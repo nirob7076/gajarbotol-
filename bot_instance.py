9202b25aba4389b66b4ec474d0d58183fef7eec9
nirob7076 <ssb43550@gmail.com>
2026-06-12 13:41:59 +0600

    Add files via upload

---
+++ bot_instance.py
@@ -0,0 +1,8 @@
+import telebot
+
+# আপনার দেওয়া বটের টোকেন সরাসরি বসানো হয়েছে
+BOT_TOKEN = "8984011233:AAG2PL7B-8Zj0iyr7uSK9KPS3C8sWd7ofxg"
+bot = telebot.TeleBot(BOT_TOKEN)
+
+# আপনার অ্যাডমিন আইডি
+ADMIN_ID = 8357251736
